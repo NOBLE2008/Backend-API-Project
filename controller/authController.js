9a1f98946b2f6df60f0e2b948a7e5290293dc256
Noble Benjamin <nobleben2008@gmail.com>
2024-05-22 00:56:05 +0100

    Implemented Password Reset Token Email 📧

--- controller/authController.js
+++ controller/authController.js
@@ -137,7 +137,7 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   <p>A password reset has been initiated. click on the button below to reset your password.</p>
   <a href="${resetUrl}"><button style="background-color: blue; color: white; padding: 10px; text-decoration: none; display: inline-block">Reset Password</button></a>`;
 
-  const emailUser  = sendEmail(email, emailSubject, emailText, emailHtml);
+  const emailUser = sendEmail(email, emailSubject, emailText, emailHtml);
   await emailUser(req, res, next);
   res.status(200).json({
     status: 'Success',
