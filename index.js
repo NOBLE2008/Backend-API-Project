c6dade17b96072d83226cc0aaa35393ebb003387
Noble Benjamin <nobleben2008@gmail.com>
2024-05-29 19:54:33 +0100

    Added a users folder for storing user images in the public/img/users directory

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const multer = require('multer');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const rateLimit = require('express-rate-limit');
 const cookieParser = require('cookie-parser')
